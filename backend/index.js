02da5acfb2f9398d085c87f66925315fa96f5621
seb-art <kimanzikims300@gmail.com>
2023-08-29 23:32:21 +0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+const server = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+server();
